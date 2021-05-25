5846925588a9a7b22eaf2866c44796aa276ea20a
Salvador Recuero <salvarecuero@gmail.com>
2021-05-25 20:44:36 -0300

    Added listen port as environment variable.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3500;
 const resolveCache = require("./handlers/resolveCache");
 
 app.use(cors());
 
 app.get("/api/get-tree/", resolveCache);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3500, () => {
   console.log(`Server running...`);
 });
